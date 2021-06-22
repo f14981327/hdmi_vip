1981c0b9c9d4f3c85cc554b6f26f051e9475112b
f14981327 <60082076+f14981327@users.noreply.github.com>
2021-06-22 12:07:19 +0800

    Create rt_hdmi_if.sv
    initial check in

---
+++ rt_hdmi_if.sv
@@ -0,0 +1,5 @@
+// hello world
+$display("lane_data_0=%h", rt_hdmi_if.lane_data_0);
+$display("lane_data_1=%h", rt_hdmi_if.lane_data_1);
+$display("lane_data_2=%h", rt_hdmi_if.lane_data_2);
+$display("lane_data_3=%h", rt_hdmi_if.lane_data_3);
