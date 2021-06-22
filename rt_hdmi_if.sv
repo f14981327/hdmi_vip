ccc4c0c5cd0b64d585320b9baf7b78a90e6359e2
f14981327 <60082076+f14981327@users.noreply.github.com>
2021-06-22 12:09:21 +0800

    Update rt_hdmi_if.sv
    create b16scr_2_16b for loop

--- rt_hdmi_if.sv
+++ rt_hdmi_if.sv
@@ -3,3 +3,7 @@ $display("lane_data_0=%h", rt_hdmi_if.lane_data_0);
 $display("lane_data_1=%h", rt_hdmi_if.lane_data_1);
 $display("lane_data_2=%h", rt_hdmi_if.lane_data_2);
 $display("lane_data_3=%h", rt_hdmi_if.lane_data_3);
+
+for (int i = 0; i < 16; i++) begin
+  b16_in = b16_scr[i];
+end
