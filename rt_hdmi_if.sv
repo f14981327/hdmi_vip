7e33b768a28982d4ef248463f23cc5305d544ad5
f14981327 <60082076+f14981327@users.noreply.github.com>
2021-06-22 12:11:24 +0800

    Update rt_hdmi_if.sv
    add special character (fee1/fee8/3011e/30117)

--- rt_hdmi_if.sv
+++ rt_hdmi_if.sv
@@ -1,4 +1,7 @@
 // hello world
+
+bit [17:0] = {18'hfee1, 18'hfee8, 18'h3011e, 18'h30117};
+
 $display("lane_data_0=%h", rt_hdmi_if.lane_data_0);
 $display("lane_data_1=%h", rt_hdmi_if.lane_data_1);
 $display("lane_data_2=%h", rt_hdmi_if.lane_data_2);
